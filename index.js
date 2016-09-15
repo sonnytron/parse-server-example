f7096e5e4693c004a891cbc0df272780fbfe7b69
Sonny Rodriguez <sonny.rodriguez@manifest.com>
2016-09-15 10:31:24 -0500

    added S3Adapter credentials

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
+var S3Adapter = require('parse-server').S3Adapter;
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
@@ -17,6 +18,12 @@ var api = new ParseServer({
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  filesAdapter: new S3Adapter(
+      "AKIAI4FLSWXHLK5WPFCA",
+      "NASIuXA17+DwYQyg7KE0lCZFZ76H9EggmiJXTi5F",
+      "manifesttv",
+      {directAccess: true}
+    ),
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
